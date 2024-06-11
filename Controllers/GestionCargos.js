d3cb939b777b431525566fe0e383d2ed314274c6
JulioMolina14 <respaldo863@gmail.com>
2024-06-10 20:00:11 -0500

    hola

--- Controllers/GestionCargos.js
+++ Controllers/GestionCargos.js
@@ -16,7 +16,7 @@ class ServicioCargos {
 
         catch (err) {
             console.error(err);
-            return ('Guardado errado en cargos revisa');
+            return ('Guardado errado ');
         }
 
     }
